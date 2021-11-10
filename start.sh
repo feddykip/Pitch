78774f8c814554df7c1b6ec61715225fdfda9670
feddykip <ferdinand.kiplangat@student.moringaschool.com>
2021-11-10 13:22:03 +0300

    added a template

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
-export MAIL_USERNAME='feddykip@gmail.com'
+    export MAIL_USERNAME='feddykip@gmail.com'
 export MAIL_PASSWORD='0729659258'
-export SECRET_KEY='powerful secretkey'
+export SECRET_KEY='powerfulsecretkey'
 
 
 python3.8 manage.py server
